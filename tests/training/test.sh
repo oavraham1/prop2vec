e206ba33ea5a3e56308fb3e3c3cf5c1a191c75da
Oded <oavraham1@gmail.com>
2017-02-14 21:41:54 +0200

    fix training test

--- tests/training/test.sh
+++ tests/training/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+ref="wiki_10k_wlm.vec"
 input="wiki_10k_wlm"
 output="model.tmp"
-ref="wiki_10k_wlm.vec"
 
-./../../training/fasttext skipgram -input ../tests/training/$input -output ../tests/training/$output -lr 0.025 -dim 200 -ws 2 -epoch 5 -minCount 5 -neg 5 -loss ns -bucket 2000000 -minn 0 -maxn 1 -thread 1 -t 1e-3 -lrUpdateRate 100
-cmp --silent $ref $output || echo "training output has changed"
+./../../training/fasttext skipgram -input $PWD/$input -output $PWD/$output -lr 0.025 -dim 200 -ws 2 -epoch 5 -minCount 5 -neg 5 -loss ns -bucket 2000000 -minn 0 -maxn 1 -thread 1 -t 1e-3 -lrUpdateRate 100
+cmp --silent $ref $output.vec || echo "training output has changed"
