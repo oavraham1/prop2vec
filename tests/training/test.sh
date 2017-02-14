8222d5ad06b27fa0006c1d425fe4373deb2b102a
Oded <oavraham1@gmail.com>
2017-02-14 20:54:24 +0200

    add training implementation

--- tests/training/test.sh
+++ tests/training/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-input="input"
-output="output.tmp"
-ref="ref"
+input="wiki_10k_wlm"
+output="model.tmp"
+ref="wiki_10k_wlm.vec"
 
-./../../training/train.sh $input $output
+./../../training/fasttext skipgram -input ../tests/training/$input -output ../tests/training/$output -lr 0.025 -dim 200 -ws 2 -epoch 5 -minCount 5 -neg 5 -loss ns -bucket 2000000 -minn 0 -maxn 1 -thread 1 -t 1e-3 -lrUpdateRate 100
 cmp --silent $ref $output || echo "training output has changed"
