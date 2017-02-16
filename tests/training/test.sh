ca8e19be2daeea8f36818e803a1fdeb3af787a2a
Oded <oavraham1@gmail.com>
2017-02-16 20:01:44 +0200

    'props' arg

--- tests/training/test.sh
+++ tests/training/test.sh
@@ -5,5 +5,5 @@ input=$PWD/wiki_10k_wlm
 output=$PWD/model.tmp
 trainer=fasttext
 
-(cd "../../training"; [ -e $trainer ] && rm $trainer; make; ./$trainer skipgram -input $input -output $output -lr 0.025 -dim 200 -ws 2 -epoch 5 -minCount 5 -neg 5 -loss ns -bucket 2000000 -thread 1 -t 1e-3 -lrUpdateRate 100)
+(cd "../../training"; [ -e $trainer ] && rm $trainer; make; ./$trainer skipgram -input $input -output $output -props "w+l+m" -lr 0.025 -dim 200 -ws 2 -epoch 5 -minCount 5 -neg 5 -loss ns -bucket 2000000 -thread 1 -t 1e-3 -lrUpdateRate 100)
 cmp --silent $ref $output.vec || echo "training output has changed"
