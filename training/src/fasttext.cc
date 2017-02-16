a3102597c3bbb3ee52718b1e6d9522cf463c3ae7
Oded <oavraham1@gmail.com>
2017-02-16 19:44:32 +0200

    no need to save model for now

--- training/src/fasttext.cc
+++ training/src/fasttext.cc
@@ -363,7 +363,7 @@ void FastText::train(std::shared_ptr<Args> args) {
   }
   model_ = std::make_shared<Model>(input_, output_, args_, 0);
 
-  saveModel();
+  //saveModel();
   if (args_->model != model_name::sup) {
     saveVectors();
   }
