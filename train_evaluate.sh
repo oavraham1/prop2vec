7c35669baf478ec335c1cb0df271bbf100c60465
Oded <oavraham1@gmail.com>
2017-02-19 17:09:17 +0200

    include props in model name

--- train_evaluate.sh
+++ train_evaluate.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+props="w+l+m"
 corpus_processed=$1
-model=$corpus_processed.vec
+model_path=${corpus_processed}_$props
+model_full_path=$model_path.vec
 
-./training/fasttext skipgram -input $corpus_processed -output $corpus_processed -props "w+l+m" -lr 0.025 -dim 200 -ws 2 -epoch 5 -minCount 5 -neg 5 -loss ns -bucket 2000000 -thread 1 -t 1e-3 -lrUpdateRate 100
+./training/fasttext skipgram -input $corpus_processed -output $model_path -props $props -lr 0.025 -dim 200 -ws 2 -epoch 5 -minCount 5 -neg 5 -loss ns -bucket 2000000 -thread 1 -t 1e-3 -lrUpdateRate 100
 
-./evaluate.sh $model
+echo $model_full_path
+./evaluate.sh $model_full_path
